dfe20a39f17fe39c4cd1f4344ec7ea25a605c0db
mackliu <macklun@gmail.comm>
2024-05-26 00:02:27 +0800

    建立相關關聯檔案

---
+++ th54j_national.sql
@@ -0,0 +1,140 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2023-06-10 07:43:00
+-- 伺服器版本： 10.4.27-MariaDB
+-- PHP 版本： 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `db15`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `acc` text NOT NULL,
+  `pw` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `admin`
+--
+
+INSERT INTO `admin` (`id`, `acc`, `pw`) VALUES
+(1, 'admin', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `bus`
+--
+
+CREATE TABLE `bus` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` text NOT NULL,
+  `minute` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `bus`
+--
+
+INSERT INTO `bus` (`id`, `name`, `minute`) VALUES
+(1, 'A12345', 0),
+(2, 'B12345', 0),
+(4, 'C12345', 0);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `station`
+--
+
+CREATE TABLE `station` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` text NOT NULL,
+  `before` int(10) UNSIGNED NOT NULL,
+  `minute` int(10) NOT NULL,
+  `waiting` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `station`
+--
+
+INSERT INTO `station` (`id`, `name`, `before`, `minute`, `waiting`) VALUES
+(1, '台北火車站', 0, 0, 5),
+(2, '台北科技大學', 3, 5, 3),
+(3, '市政府捷運站', 2, 4, 3),
+(4, '松山火車站', 1, 3, 5),
+(5, '南港高中', 4, 2, 3),
+(6, '南港火車站', 5, 3, 5),
+(7, '南港高工', 6, 8, 3),
+(9, '南港展覽館', 7, 3, 5),
+(10, '西門町', 10, 6, 3);
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `bus`
+--
+ALTER TABLE `bus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `station`
+--
+ALTER TABLE `station`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `bus`
+--
+ALTER TABLE `bus`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `station`
+--
+ALTER TABLE `station`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
