aea8833ede4a8aceaac3bf1f9d5ff5d0cc705cfc
mackliu <macklun@gmail.com>
2024-06-02 16:27:18 +0800

    更新資料庫內容

--- th54j_national.sql
+++ th54j_national.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2023-06-10 07:43:00
--- 伺服器版本： 10.4.27-MariaDB
--- PHP 版本： 8.2.0
+-- 產生時間： 2024-06-02 10:26:42
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `db15`
+-- 資料庫： `db03`
 --
 
 -- --------------------------------------------------------
@@ -57,9 +57,9 @@ CREATE TABLE `bus` (
 --
 
 INSERT INTO `bus` (`id`, `name`, `minute`) VALUES
-(1, 'A12345', 0),
-(2, 'B12345', 0),
-(4, 'C12345', 0);
+(1, 'A12345', 31),
+(2, 'B12345', 24),
+(4, 'C12345', 6);
 
 -- --------------------------------------------------------
 
@@ -70,7 +70,7 @@ INSERT INTO `bus` (`id`, `name`, `minute`) VALUES
 CREATE TABLE `station` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` text NOT NULL,
-  `before` int(10) UNSIGNED NOT NULL,
+  `rank` int(10) UNSIGNED NOT NULL,
   `minute` int(10) NOT NULL,
   `waiting` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -79,16 +79,15 @@ CREATE TABLE `station` (
 -- 傾印資料表的資料 `station`
 --
 
-INSERT INTO `station` (`id`, `name`, `before`, `minute`, `waiting`) VALUES
-(1, '台北火車站', 0, 0, 5),
-(2, '台北科技大學', 3, 5, 3),
-(3, '市政府捷運站', 2, 4, 3),
-(4, '松山火車站', 1, 3, 5),
-(5, '南港高中', 4, 2, 3),
-(6, '南港火車站', 5, 3, 5),
-(7, '南港高工', 6, 8, 3),
-(9, '南港展覽館', 7, 3, 5),
-(10, '西門町', 10, 6, 3);
+INSERT INTO `station` (`id`, `name`, `rank`, `minute`, `waiting`) VALUES
+(1, '台北火車站', 1, 0, 5),
+(2, '台大醫院', 2, 2, 3),
+(3, '中正紀念堂', 3, 2, 3),
+(4, '東門', 4, 3, 5),
+(5, '大安森林公園', 5, 2, 3),
+(6, '大安', 6, 1, 5),
+(7, '信義安和', 7, 3, 3),
+(8, '台北101', 8, 2, 5);
 
 --
 -- 已傾印資料表的索引
@@ -126,13 +125,13 @@ ALTER TABLE `admin`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `bus`
 --
 ALTER TABLE `bus`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `station`
 --
 ALTER TABLE `station`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
