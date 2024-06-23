f0e8b77a6a988b3aef6868fe1bc1cddfb81cf86d
mackliu <macklun@gmail.com>
2024-06-23 14:55:13 +0800

    修改/page/edit_user.php的表單欄位定義

--- pages/edit_user.php
+++ pages/edit_user.php
@@ -6,12 +6,12 @@ $user=$pdo->query("SELECT * FROM `users` WHERE `id`='{$_GET['id']}'")->fetch();
     <div class="row w-100">
         <label for="" class="col-2">姓名</label>   
         <input  type="text" name="name" id="name" value="<?=$user['name'];?>" class='form-group form-control col-10'
-                min='0' step="1" required>
+                >
     </div>
     <div class="row w-100">
         <label for="" class="col-2">接駁車</label>   
         <input  type="text" name="bus" id="bus" value="<?=$user['bus'];?>" class='form-group form-control col-10'
-                min='0' step="1" required>
+                >
     </div>
     <div class="row w-100">
         <input  type="button" value="修改" class='col-12 btn btn-success my-1' onclick="save()">
