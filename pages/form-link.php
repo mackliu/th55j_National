981b6c44dfc3cbd4f032e5bd0afa1e53b8c2a665
mackliu <macklun@gmail.com>
2025-04-26 16:08:18 +0800

    建立表單設定頁面

--- pages/form-link.php
+++ pages/form-link.php
@@ -3,40 +3,41 @@
     <h1 class="border p-3 text-center my-3">表單管理 
         <button class="btn btn-success" onclick="load('add_user.php')">新增參與者email</button>
     </h1>
-    <div class='d-flex justify-content-between align-items-center p-2'>
-            <!-- 取得表單啟用狀態 -->
+    <div class='d-flex justify-content-between align-items-top p-2'>
+      <div id="form-sidebar" class="col-2 border-right">
+        <div id="basic-setting-link" class="m-1 text-center px-3 py-2 bg-light border">基本設定</div>
+        <div id="responses-setting-link" class="m-1 text-center px-3 py-2 border">檢視回應</div>
+        <div id="stastic-seeting-link" class="m-1 text-center px-3 py-2 border">統計資料</div>
+      </div>
+      <div id="form-page" class="col-10">
+        <div>
+            <h3>基本設定</h3>
+            <div class="d-flex">
+                <span>啟用表單</span>
+                <div class="custom-control custom-switch mx-1">
+                    <input type="checkbox" name="enable" class="custom-control-input" id="active-form" checked>
+                    <label class="custom-control-label" for="active-form">啟用</label>
+                </div>
+            </div>
+            <label for="">起始時間</label>
+            <input type="datetime-local" name="start-at" id="start-at" class="form-control">
+            <label for="">結束時間</label>
+            <input type="datetime-local" name="end-at" id="end-at" class="form-control">
+
+            
+            <button class="w-100 btn btn-primary m-2" id="save-button">儲存</button>
+        </div>
+        <div>
+            <h3>檢視回應</h3>
+        </div>
+        <div>
+            <h3>統計資料</h3>
+        </div>
+      </div>      
         
-        <div>
-            表單啟用狀態：<button class="btn btn-primary my-3" id="form_status" onclick="" data-status=""></button>
-        </div>
-        <div>
-            <!-- 取得接駁車人數 -->
-            
-            
-        </div>
-        <div>
-            <!-- 取得預計接駁車數量 -->
-            
-            預計接駁車數量：<button class="btn btn-success font-weight-border"></button>
-            <button class="btn btn-info ml-3" onclick="">分配接駁車</button>
-        </div>
-    </div>
-    <h2 class="text-center border-bottom pb-2 mt-2">參與者清單</h2>
-    <!-- 參與者清單區塊，使用overflow屬性來建立一個有高度限制的區域放置參與者清單-->
-    <div id="users" class=" d-flex align-items-start align-content-start flex-wrap w-100" style='overflow:auto;height:25rem'>
-            
-    </div>
+     
 
-    <h2 class="text-center border-bottom pb-2 mt-2">意願調查結果</h2>
-    <!-- 意願調查結果區塊 -->
-    <div id="surveys" class="d-flex align-items-start align-content-start flex-wrap w-100" style='overflow:auto;height:25rem'>
 
-    </div>
-
-    <h2 class="text-center border-bottom pb-2 mt-2">接駁車分派結果</h2>
-    <!-- 意願調查結果區塊 -->
-    <div id="busses" class="d-flex align-items-start align-content-start flex-wrap w-100" style='overflow:auto;height:25rem'>
-        <h3 class="text-center w-100">尚未分派接駁車</h3>
     </div>
 </div>
 
