c182d937a7f953863791bb3058ea96625f7590b5
mackliu <macklun@gmail.com>
2025-05-03 10:25:04 +0800

    更改路線新增為checkbox

--- api/add_route.php
+++ api/add_route.php
@@ -2,10 +2,10 @@
 include_once "db.php";
 
 //建立新增路線資料用的SQL語法
-q("insert into `route` (`name`)  values ('{$_POST['name']}')");
+q("insert into `route` (`name`)  values ('{$_POST['routeName']}')");
 
 //取得剛新增的路線ID
-$route_id=q("select `id` from `route` where `name`='{$_POST['name']}'")[0]['id']; //取得剛新增的路線ID
+$route_id=q("select `id` from `route` where `name`='{$_POST['routeName']}'")[0]['id']; //取得剛新增的路線ID
 
 //先將$_POST['stations']中的資料依seq 由小到大排序
 usort($_POST['stations'], function($a, $b) {
@@ -15,5 +15,5 @@ usort($_POST['stations'], function($a, $b) {
 //建立新增路線站點關聯表的SQL語法
 foreach($_POST['stations'] as $station){
     q("insert into `route_station` (`route_id`, `station_id`, `arriving_time`, `staying_time`, `seq`) 
-        values ('{$route_id}', '{$station['station_id']}', '{$station['arriving_time']}', '{$station['staying_time']}', '{$station['seq']}')");
+        values ('{$route_id}', '{$station['stationId']}', '{$station['arrivingTime']}', '{$station['stayingTime']}', '{$station['seq']}')");
 }
--- pages/add_route.php
+++ pages/add_route.php
@@ -4,26 +4,13 @@
         <label for="" class="col-2">路線名稱</label>
         <input type="text" name="route" id="route" class='form-group form-control col-10'>
         <div class="row w-100">
-            <div class="col-6">
+            <div class="col-10 m-auto">
                 <div class="text-center">選擇站點</div>
-                <div id="station-list" class="d-flex flex-wrap align-items-center"></div>
-            </div>
-            <div class="col-6">
-                <div class="text-center">編輯站點</div>
-                <div class="list-group">
-                    <div class="d-flex justify-content-between align-items-center list-group-item" data-id="${stationId}">
-                        <div class="d-flex col-10">
-                            <div class="col-6 px-1">站點名稱</div>
-                            <div class="col-3 px-1">行駛時間</div>
-                            <div class="col-3 px-1">停留時間</div>
-                        </div>
-                        <div class="col-1">&nbsp</div>
-                    </div>
-                </div>
-                <div id="selected-stations" class="list-group">
+                <div id="station-list">
 
                 </div>
             </div>
+            
         </div>
     </div>
     <div class="row w-100">
@@ -40,8 +27,23 @@
 
     //當按下新增路線按鈕時，將路線名稱及選擇的站點資料傳送到api/add_route.php
     $("#add-button").on("click",function(){
+        $(".station-chk:checked").each(function(){
+            
+            let station={
+                stationId:$(this).data('station-id'),
+                stationName:$(this).data('station-name'),
+                arrivingTime:$(this).siblings('.arriving-time').val(),
+                stayingTime:$(this).siblings('.staying-time').val(),
+                seq:$(this).siblings('.seq').val()
+            }
+
+            selectedStations.push(station)
+        })
+
+        //console.log(selectedStations);
+
         $.post("./api/add_route.php",{
-                name: $("#route").val(),
+                routeName: $("#route").val(),
                 stations:selectedStations
             } ,(res) => {
                 //console.log(res)
@@ -56,6 +58,7 @@
         setActive('route-link')
     })
 
+    //載入所有站點
     $.get("./api/get_stations.php", (data) => {
         stations.length=0;   //先清空陣列
 
@@ -66,104 +69,14 @@
 
         //列出站點
         listStations(stations);
-        //將要加入路線的站點資料加入到selectedStations陣列中，並在編輯區域顯示
-        $("#station-list").on("click",".btn-primary",function(){
-            //取得站點資料
-            let stationName=$(this).data("station-name")
-            let stationId=$(this).data("station-id")
+        
 
-            //找出selectedStations中最大的seq值+1來做為新站點的seq值
-            //如果selectedStations中還沒有資料，則seq=1
-            let seq=0;
-            if(selectedStations.length==0){
-                seq=1
-            }else{
-                seq=Math.max(...selectedStations.map(station=>station.seq))+1
-            }
-
-           //站點資料加入到selectedStations陣列中
-            selectedStations.push({
-                station_id: stationId,
-                station_name: stationName,
-                arriving_time: 1,
-                staying_time: 1,
-                seq: seq
-            })
-            //在stations陣列中移除已選擇的站點
-            stations=stations.filter(station=>station.id!=stationId)
-
-            //移除站點
-            $(this).parent().parent().remove()
-
-            //將選擇的站點資料顯示在編輯區域
-            listSelectedStations(selectedStations)
-            //console.log(stations,selectedStations)
-        })
-
-        $("#selected-stations")
-            .on("click",".move-up",function(){ //綁定上移事件
-                let seq=$(this).parents(".selected-item").data("seq")
-                let prevItem=$(this).parents(".selected-item").prev()
-                if(prevItem.length>0){
-                    $(this).parents(".selected-item").insertBefore(prevItem)
-                    $(this).parents(".selected-item").data("seq",seq-1)
-                    $(prevItem).data("seq",seq)
-                    //更新selelctedStations陣列中的seq值
-                    //取得目前站點的在selectedStations中的index值
-                    let index=selectedStations.findIndex(station=>station.station_id==$(this).parents(".selected-item").data("id"))
-                    selectedStations[index].seq=seq-1
-
-                    //取得上移的站點在selectedStations中的index值
-                    index=selectedStations.findIndex(station=>station.station_id==$(prevItem).data("id"))
-                    selectedStations[index].seq=seq
-                }
-                //console.log(selectedStations)
-        })
-        .on("click",".move-down",function(){  //綁定下移事件
-            let seq=$(this).parents(".selected-item").data("seq")
-                let nextItem=$(this).parents(".selected-item").next()
-                if(nextItem.length>0){
-                    $(this).parents(".selected-item").insertAfter(nextItem)
-                    $(this).parents(".selected-item").data("seq",seq+1)
-                    $(nextItem).data("seq",seq)
-                    //更新selelctedStations陣列中的seq值
-                    //取得目前站點的在selectedStations中的index值
-                    let index=selectedStations.findIndex(station=>station.station_id==$(this).parents(".selected-item").data("id"))
-                    selectedStations[index].seq=seq+1
-                    //取得下移的站點在selectedStations中的index值
-                    index=selectedStations.findIndex(station=>station.station_id==$(nextItem).data("id"))
-                    selectedStations[index].seq=seq
-                }
-                //console.log(selectedStations)
-        })
-        .on("click",".btn-danger",function(){  //綁定刪除事件
-            let id=$(this).parents(".selected-item").data("id")
-            let name=$(this).parent().find(".col-6").text()
-            let index=selectedStations.findIndex(station=>station.station_id==id)
-            //將刪除的站點資料從selectedStations陣列中移除
-            selectedStations.splice(index,1)
-            //重新列表選擇的站點資料
-            listSelectedStations(selectedStations)
-            
-            //將刪除的站點重新加入到stations陣列中
-            stations.push({
-                id: id,
-                name: name
-            })
-            //更新選擇的站點資料顯示
-            listStations(stations)
-        })
-        .on("change","input",function(){  //綁定輸入框變更事件
-            let id=$(this).parents(".selected-item").data("id")
-            let name=$(this).parents(".selected-item").data("name")
-            let index=selectedStations.findIndex(station=>station.station_id==id)
-
-            //將輸入框(行駛時間及停留時間)的值存入selectedStations陣列中
-            selectedStations[index][$(this).attr("name")]=$(this).val()
-        })
+        
     });
 
-
+    /**
+     * 列出所有站點
+     */
     function listStations(stations){
         //先清空站點選擇區域的內容
         $("#station-list").empty()
@@ -171,17 +84,28 @@
         //取得所有站點資料並在站點選擇區域顯示
         stations.forEach(station=>{
             $("#station-list").append(`
-            <div class="col-4 p-1 ">
-                <div class="d-flex justify-content-between align-items-center btn btn-info">
-                    <span>${station.name}</span>
-                    <button class="station-btn btn btn-primary btn-sm" data-station-name="${station.name}" data-station-id="${station.id}">+</button>
-                </div>
+            <div class="station-select col-12 p-1  d-flex ">
+                <input type='checkbox' class="station-chk" data-station-name="${station.name}" data-station-id="${station.id}" />
+                <span>${station.name}</span>
+                
+
             </div>
             `)
         })
+
+        $(".station-chk").on("click",function(){
+            $(this).parent().append(
+                `
+                <input type='number' class='arriving-time form-control mx-1 col-3' placeholder="行駛時間" name='arriving_time'  value=''>
+                <input type='number' class='staying-time form-control mx-1 col-3' placeholder="停留時間" name='staying_time'  value=''>
+                <input type='number' class='seq form-control mx-1 col-3' placeholder='順序' name='seq'  value=''>
+                `
+            )
+        })
+
     }
 
-    function listSelectedStations(selectedStations){
+   /*  function listSelectedStations(selectedStations){
         //先清空編輯區域的內容
         $("#selected-stations").empty()
 
@@ -205,7 +129,7 @@
                 </div>
             `)
         })
-    }
+    } */
     })
     
 </script>
