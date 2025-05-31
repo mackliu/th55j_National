7cbcf3b75598342a971719784fbf475cee896480
mackliu <macklun@gmail.comm>
2025-05-31 15:33:10 +0800

    1. 簡化svg繪製的程式碼 2. 修正了一列只有一個站點時的svg繪製錯誤問題

--- index.php
+++ index.php
@@ -48,7 +48,11 @@
     align-items:flex-end;
 }
 
-/**建立站點的外型圓點 */
+/**
+ * 建立站點的外型圓點 
+ * 如果題目沒有要求,也可以直接用icon圖片來代替
+ * 這裡使用CSS來建立圓點，並使用::before特性來建立圓點中的白圈
+ * */
 .station-point{
     width:25px;
     height:25px;
@@ -123,7 +127,7 @@
         <div class="d-flex flex-column">
             <label for="route-select">請選擇路線</label>
             <select name="route-select" id="route-select" class="form-control">
-                
+                <!--使用ajax來取得所有路線資料，並加入到這個select元素中-->
             </select>
         </div>
     </div>
@@ -131,15 +135,16 @@
 
 <!--路網圖區塊-->
 <div class="d-flex flex-wrap my-4 mx-auto shadow p-5" style='width:min-content' id="route-map">
-
+<!--這個區塊將會使用ajax來取得所有站點資料，並繪製路網圖-->
+    
 </div>
 </div>
 <script src="./js/jquery.js"></script>
 <script src="./js/bootstrap.js"></script>
 <script>
 
-//設定每一列的站點數量
-let PointsPerRow=3;
+//設定每一列的站點數量，如果現場題目有指定前端站點數量可變更，則此變數可從後端取得
+let PointsPerRow=4;
 
 
 getRoutes()
@@ -150,20 +155,27 @@ getRoutes()
 function getRoutes(){
     //使用ajax來取得所有路線資料
     $.get("./api/get_routes.php",(routes)=>{
+
         //將路線資料加入到id為route-select的select元素中
         routes.forEach(route => {
+
+            //使用模板字串來建立option元素，其中的value為路線ID，顯示的文字為路線名稱
             $("#route-select").append(`
                 <option value="${route.id}">${route.name}</option>
             `)
         })
 
-        //取出第一個路線的資料，並繪製路網圖
+        //取出第一個路線的資料，並繪製路網圖(預設顯示的路線)
         getStations($("#route-select").val()) //取得選擇的路線資料
 
     })
 }
+
+
 //當路線選擇改變時，重新取得該路線的所有站點資料，並繪製路網圖
 $("#route-select").on("change",function(){
+
+    //取得選擇的路線ID($(this).val())，並使用getStations函式來取得該路線的所有站點資料
     getStations($(this).val())
 })
 
@@ -174,11 +186,11 @@ $("#route-select").on("change",function(){
 function getStations(id){
     //使用ajax來取得所有站點資料
     $.get("./api/get_route_stations.php",{id},(stations)=>{
-        console.log(stations)
-        //建立一個變數來計算目前列數
+        //console.log(stations)
+        //建立一個變數來計算目前列數，起始值為0
         let row=0;
 
-        //計算總共有幾列
+        //計算總共有幾列，站點數量除以每一列的站點數量 
         let totalRows=Math.floor(stations.length/PointsPerRow);
 
         //建立一個變數map來儲存路網圖的HTML內容
@@ -190,7 +202,7 @@ function getStations(id){
             //建立一個變數來判斷是否要反轉站點的排列方式，預設為空值
             let reverse='';
             
-            //使用餘數來判斷是否為每一列的第一個站點
+            //使用餘數(索引值%每列站點數)來判斷是否為每一列的第一個站點
             if(idx%PointsPerRow==0){
 
                 //判斷是否為奇數列，如果是奇數列則反轉站點排列方式
@@ -262,16 +274,43 @@ function drawPath(){
     //取得所有的站點
     let points=$(".station-point")
 
-    //取得單一站點區塊的寬高
-    let stationWidth=$(".station").width()
-    let stationHeight=$(".station").height()
-
     //取得路網圖的區塊資訊(包含位置及大小)
-    routeMapRect=$("#route-map").get(0).getBoundingClientRect()
+    let roteMapRect=$("#route-map").get(0).getBoundingClientRect()
 
-    //建立一個變數來儲存路徑的字串
+    //建立一個變數來儲存路徑的字串，這個字串將會用來繪製SVG的路徑
     let path='M'
+
+    //如果每列僅一個站點，則直接將每個點連線即可
+    if(PointsPerRow==1){
+        points.each(function(idx,point){
+            let rect=$(point).get(0).getBoundingClientRect()
+            let x=rect.left + rect.width / 2 - roteMapRect.left
+            let y=rect.top + rect.height / 2 - roteMapRect.top
+            if(idx===0){
+                //如果是第一個站點，則使用M來開始路徑
+                path+=` ${x} ${y}`
+            }else{
+                //如果不是第一個站點，則使用L來連線
+                path+=` L ${x} ${y}`
+            }
+            
+        });
+        $("#route-path").attr("d",path) //將路徑字串加入到SVG的path中
+        return; //結束函式
+    }
+
+    //如果每列有多個站點，則需要計算每個站點的座標及轉彎點
+    //取得單一站點區塊的寬高，每一塊都是一樣的，所以只取第一個站點的寬高即可
+    let stationWidth=$(".station").eq(0).width()
+    let stationHeight=$(".station").eq(0).height()
+
+    //console.log(stationWidth,stationHeight)
+    routeMapRect=$("#route-map").get(0).getBoundingClientRect()
+    //console.log(routeMapRect)
     
+    //計算總列數，使用Math.ceil來確保即使有餘數也能計算出完整的列數
+    let totalRows=Math.ceil(points.length/PointsPerRow)
+
     //使用迴圈來逐一取得每一個站點的座標
     points.each(function(idx,point){
         /**
@@ -282,33 +321,37 @@ function drawPath(){
          */
         let rect=$(point).get(0).getBoundingClientRect()
         let x,y,x1,y1
-        if(idx==0){
-            //如果是第一個站點，計算圓點中心的x,y座標點
-            x=rect.left + rect.width / 2 - routeMapRect.left
-            y=rect.top + rect.height / 2 - routeMapRect.top
-            path+=` ${x} ${y}`
-        }else if(idx==points.length-1){
-            //如果是最後一個站點，計算圓點中心的x,y座標點
-            x=rect.left + rect.width / 2 - routeMapRect.left
-            y=rect.top + rect.height / 2 - routeMapRect.top
-            path+=`L ${x} ${y} `
-        }else if(idx%PointsPerRow==PointsPerRow-1){
-            //如果是每一列的最後一個站點
 
+        //先計算目前站點的x,y座標點
+        x=rect.left + rect.width / 2 - routeMapRect.left
+        y=rect.top + rect.height / 2 - routeMapRect.top
+
+        if(idx==0){
+
+            //如果是第一個站點，直接加入到路徑字串中
+            path+=` ${x} ${y}`
+
+        }else if(idx==points.length-1){
+
+            //如果不是第一個站點，加入'L'來連線
+            path+=`L ${x} ${y} `
+
+        }else if(idx%PointsPerRow==PointsPerRow-1){
+            
+            //如果是每一列的最後一個站點
             if((Math.floor(idx/PointsPerRow))%2==0){
                 //如果是偶數列(包含第0列)，則計算出向右延伸一半寬度的x座標點
-                x=rect.left + rect.width / 2 - routeMapRect.left + stationWidth/2
+                x=x + stationWidth/2
             }else{
                 //如果是奇數列，則計算出向左延伸一半寬度的x座標點
-                x=rect.left + rect.width / 2 - routeMapRect.left - stationWidth/2
+                x=x - stationWidth/2
             }
-                y=rect.top + rect.height / 2 - routeMapRect.top //計算y座標點
                 x1=x  //計算向下延伸的x座標點
                 y1=rect.top + rect.height / 2 - routeMapRect.top + stationHeight //計算下一行的y座標點
 
                 //將直角轉彎的兩個座標點加入到路徑字串中
                 path+=`L ${x} ${y} L ${x1} ${y1} `
-        }        
+        }      
 
     })
     //將路徑字串加入到SVG的path中
